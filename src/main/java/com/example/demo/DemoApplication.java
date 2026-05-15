f6afbc69a8edbf48d1904204fa4adaba7bffd546
MontesNancy <160776780+MontesNancy@users.noreply.github.com>
2026-05-15 13:59:16 -0600

    Update greeting message in home method

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -12,7 +12,7 @@ public class DemoApplication {
 
     @RequestMapping("/")
     String home() {
-        return "Hello World!";
+        return "Hello World 15/05/2026!";
     }
 
     public static void main(String[] args) {
